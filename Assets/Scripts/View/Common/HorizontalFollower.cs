e4dac7446523e12025179050b3f2d596b7e1097b
in3orn <in3orn@gmail.com>
2018-11-21 18:43:36 +0100

    Basic camera follower

---
+++ Assets/Scripts/View/Common/HorizontalFollower.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Krk.Bum.View.Common
+{
+    public class HorizontalFollower : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform target = null;
+
+        [SerializeField]
+        private HorizontalFollowerConfig config = null;
+
+
+        private void Update()
+        {
+            var targetX = target.position.x;
+            var currentX = transform.position.x;
+            var diff = targetX - currentX;
+            if(Mathf.Abs(diff) > config.MinFollowDistance)
+            {
+                currentX = Mathf.Lerp(currentX, targetX, config.FollowStrength * Time.deltaTime);
+                transform.position = new Vector3(currentX, transform.position.y, transform.position.z);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/View/Common/HorizontalFollowerConfig.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Krk.Bum.View.Common
+{
+    [CreateAssetMenu(menuName = "Krk/View/Common/Follower")]
+    public class HorizontalFollowerConfig : ScriptableObject
+    {
+        public float MinFollowDistance;
+        public float FollowStrength;
+    }
+}
