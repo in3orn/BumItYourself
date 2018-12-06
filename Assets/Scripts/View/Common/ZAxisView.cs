0484422cd531a97c9cf59239916bc033e19311f3
in3orn <in3orn@gmail.com>
2018-12-06 21:16:56 +0100

    Street minor fixes
    - wrong z-positions
    - too much vertices (tiled pixel)

---
+++ Assets/Scripts/View/Common/ZAxisView.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Krk.Bum.View.Common
+{
+    public class ZAxisView : MonoBehaviour
+    {
+        private void Start()
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
+        }
+    }
+}
