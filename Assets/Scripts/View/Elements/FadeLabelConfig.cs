6f49240b799218771e62de3f714fcdc182d3417f
in3orn <in3orn@gmail.com>
2018-12-12 18:12:46 +0100

    +100$ label in store

--- Assets/Scripts/View/Elements/FadeLabelConfig.cs
+++ Assets/Scripts/View/Elements/FadeLabelConfig.cs
@@ -1,4 +1,5 @@
-﻿using Krk.Bum.View.Animations;
+﻿using System;
+using Krk.Bum.View.Animations;
 using UnityEngine;
 
 namespace Krk.Bum.View.Elements
@@ -6,8 +7,9 @@ namespace Krk.Bum.View.Elements
     [CreateAssetMenu(menuName = "Krk/View/Elements/Fade Label")]
     public class FadeLabelConfig : ScriptableObject
     {
-        public FadeAnimationConfig Fade;
+        public string Format = "+{0}";
 
+        public FadeAnimationConfig Fade;
         public MoveAnimationConfig Move;
     }
 }
